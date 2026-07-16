3426537e585e4a1dc9e368796d53d54fca4962bd
lavanyapallapu <lavanyapallapu448@gmail.com>
2026-07-16 06:27:37 +0530

    Day 1: HelloWorld and Variables programs

---
+++ Day - 01/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    
+    public static void main(String[] args)
+    {
+        int age = 21;
+        
+        String name = "Lavanya";
+
+        char initial = 'P';
+
+        double cgpa = 7.0;
+
+        boolean info = true;
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(initial);
+        System.out.println(cgpa);
+        System.out.println(info);
+    }
+}
