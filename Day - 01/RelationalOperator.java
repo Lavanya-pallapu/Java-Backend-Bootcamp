5be6a11b11807c3633ca97ed4553ddd7dad9efbd
lavanyapallapu <lavanyapallapu448@gmail.com>
2026-07-16 21:50:10 +0530

    Add RelationalOperator file

---
+++ Day - 01/RelationalOperator.java
@@ -0,0 +1,16 @@
+public class RelationalOperator {
+    
+    public static void main(String[] args){
+
+        int a = 200;
+        int b = 300;
+
+        System.out.println("Greater than : " + (a > b));
+        System.out.println("Less than : " + (a < b));
+        System.out.println("Equal to  : " + (a == b));
+        System.out.println("Not Equal to : " + (a != b));
+        System.out.println("Greater than or Equal to : " + (a >= b));
+        System.out.println("Less than or Equal to : " + (a <= b));
+
+    }
+}
