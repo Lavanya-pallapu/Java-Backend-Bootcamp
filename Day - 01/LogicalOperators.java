9d78219ec41d4b61f7ed5ba4a9ab7515ecf962dc
lavanyapallapu <lavanyapallapu448@gmail.com>
2026-07-16 22:12:18 +0530

    Add LogicalOperators file

---
+++ Day - 01/LogicalOperators.java
@@ -0,0 +1,14 @@
+public class LogicalOperators {
+
+    public static void main(String[] args){
+
+        int a = 22;
+        boolean haslicense = true;        
+
+        System.out.println(a >=18 && haslicense);
+        System.out.println(a <=18  || haslicense);
+        System.out.println(!haslicense);
+        
+    }
+    
+}
