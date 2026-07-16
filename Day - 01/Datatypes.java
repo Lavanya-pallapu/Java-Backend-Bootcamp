0592a74eaa2783a081d1836620fdb7141b373092
lavanyapallapu <lavanyapallapu448@gmail.com>
2026-07-16 15:29:14 +0530

    Add Datatypes file

---
+++ Day - 01/Datatypes.java
@@ -0,0 +1,23 @@
+public class Datatypes{
+
+    public static void main(String[] args){
+
+        byte age = 22;
+        char grade = 'A';
+        double cgpa = 7.0;
+        float percentage = 70.0f;
+        boolean passed = true;
+        short year = 2026;
+        long mobile_no = 9243556522L;
+        int salary = 50000;
+
+        System.out.println("Age : " + age);
+        System.out.println("Grade : " + grade);
+        System.out.println("CGPA : " + cgpa);
+        System.out.println("Percentage : " + percentage);
+        System.out.println("Passed : " + passed);
+        System.out.println("Year : " + year);
+        System.out.println("Mobile Number : " + mobile_no);
+        System.out.println("Salary : " + salary);
+    }
+}
